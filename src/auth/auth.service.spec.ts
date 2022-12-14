c8e2461ebfe3f6706db5770cc25aebfbb57cf4fd
박건우 <clcchosun0509@naver.com>
2022-12-15 06:21:19 +0900

    feat: add auth.service.spec

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from '../entities';
+import { UserService } from '../user/user.service';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUserService: Partial<UserService>;
 
   beforeEach(async () => {
+    fakeUserService = {
+      findOneById: (id: string) => {
+        return Promise.resolve({
+          id,
+          email: 'test@test.com',
+          username: 'test_username',
+          avatar: 'http://test.com/test.jpg',
+          roles: [],
+          createdAt: new Date('2022-12-10'),
+          deletedAt: null,
+        });
+      },
+      create: (user: User) => {
+        return Promise.resolve(user);
+      },
+    };
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UserService, useValue: fakeUserService },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -14,5 +36,10 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('validate create new user and return created user if user is not found', () => {
+    fakeUserService.findOneById = () => null;
+    expect(fakeUserService.create).toHaveBeenCalledTimes(0);
   });
 });
